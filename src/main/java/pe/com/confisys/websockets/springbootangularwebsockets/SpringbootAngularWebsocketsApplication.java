52ec74d0f0080ce1ca49334ca831b02056e24319
Jose Tejada <jtejadavilca@gmail.com>
2020-07-05 02:39:07 -0500

    Proyecto spring boot websocket - server de angular con socket

---
+++ src/main/java/pe/com/confisys/websockets/springbootangularwebsockets/SpringbootAngularWebsocketsApplication.java
@@ -0,0 +1,13 @@
+package pe.com.confisys.websockets.springbootangularwebsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularWebsocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularWebsocketsApplication.class, args);
+	}
+
+}
